46eddea3c97681e77feb406ef88626333ab23e7d
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2020-11-23 08:35:30 -0500

    added caching for histogram loading

--- scripts/agu2020_solarcycle/agu2020.py
+++ scripts/agu2020_solarcycle/agu2020.py
@@ -4,7 +4,50 @@ Script covering the entire histogram workflow process.
 """
 import os
 import datetime
+import bz2
+import pickle
+
+import hashlib
+
 import harc_plot
+from harc_plot import visualize_histograms as vh
+
+
+def hash_rd(run_dict):
+    """
+    Quick hash of the run dictionary for checking that pickle files are up-to-date.
+    """
+    rd_ = repr(sorted(run_dict.items())).encode('utf-8')
+    m   = hashlib.sha1()
+    m.update(rd_)
+    return m.hexdigest()
+
+
+def load_nc_cache(rd):
+    rd_hash     = hash_rd(rd)
+    sTime_str   = rd['sTime'].strftime('%Y%m%d')
+    eTime_str   = rd['eTime'].strftime('%Y%m%d')
+    fname       = '{!s}-{!s}.{!s}.p.bz2'.format(sTime_str,eTime_str,rd_hash)
+
+    cache_dir   = os.path.join(rd['baseout_dir'],'cache')
+    fpath       = os.path.join(cache_dir,fname)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    if os.path.exists(fpath):
+        print('Loading cached file: {!s}'.format(fpath))
+        with bz2.BZ2File(fpath,'rb') as fl:
+            nc_obj = pickle.load(fl)
+    else:
+        nc_obj              = vh.ncLoader(**rd)
+        nc_obj.rd_original  = rd
+
+        print('Saving cached file: {!s}'.format(fpath))
+        with bz2.BZ2File(fpath,'wb') as fl:
+            pickle.dump(nc_obj,fl)
+
+    return nc_obj
 
 #run_name    = 'Europe'
 run_name    = 'World'
@@ -13,49 +56,17 @@ plot_dir    = os.path.join('output/galleries/solarcycle_3hr_250km',run_name)
 #params      = ['spot_density']
 xkeys       = ['ut_hrs','slt_mid']
 #xkeys       = ['ut_hrs']
-sTime       = datetime.datetime(2009,1,1)
-eTime       = datetime.datetime(2019,1,1)
+#sTime       = datetime.datetime(2009,1,1)
+#eTime       = datetime.datetime(2020,1,1)
 
-#sTime       = datetime.datetime(2018,1,1)
-#eTime       = datetime.datetime(2018,1,31)
+sTime       = datetime.datetime(2018,1,1)
+eTime       = datetime.datetime(2018,1,31)
 
 region      = run_name
 rgc_lim     = (0, 10000)
 
 #geo_env     = harc_plot.GeospaceEnv()
 geo_env = None
-
-## Create histogram NetCDF Files ################################################
-#rd  = {}
-#rd['sDate']                 = sTime
-#rd['eDate']                 = eTime
-#rd['params']                = params
-#rd['xkeys']                 = xkeys
-#rd['rgc_lim']               = rgc_lim
-#rd['filter_region']         = run_name
-#rd['filter_region_kind']    = 'mids'
-#rd['xb_size_min']           = 24*60.
-#rd['yb_size_km']            = 250.
-#rd['loc_sources']           = None
-#rd['reprocess']             = False
-#rd['output_dir']            = data_dir
-#rd['band_obj']              = harc_plot.gl.BandData()
-#harc_plot.calculate_histograms.main(rd)
-
-## Calculate Statistics from Histograms #########################################
-#rd = {}
-#rd['src_dir']               = data_dir
-#rd['params']                = params
-#rd['xkeys']                 = xkeys
-#rd['stats']                 = ['sum','mean','median','std']
-#harc_plot.statistics_histograms.main(rd)
-
-## Baseline daily observations against statistics ###############################
-#rd = {}
-#rd['src_dir']               = data_dir
-#rd['xkeys']                 = xkeys
-#rd['stats']                 = ['pct_err','z_score']
-#harc_plot.baseline_histograms.main(rd)
 
 # Visualization ################################################################
 ### Visualize Observations
@@ -73,17 +84,12 @@ rd['plot_goes']             = False
 rd['log_z']                 = False
 rd['band_keys']             = [28, 21, 14, 7, 3, 1]
 rd['xkeys']                 = xkeys
-harc_plot.visualize_histograms.main(rd)
-#harc_plot.visualize_histograms.plot_dailies(rd)
 
-#
-#### Visualize Baselines
-#rd['srcs']                  = os.path.join(data_dir,'*.baseline_compare.nc.bz2')
+nc = load_nc_cache(rd)
+nc.plot(**rd)
+
+import ipdb; ipdb.set_trace()
 #harc_plot.visualize_histograms.main(rd)
 #harc_plot.visualize_histograms.plot_dailies(rd)
-#
-#### Visualize Statistics
-#rd = {}
-#rd['srcs']                  = os.path.join(data_dir,'stats.nc.bz2')
-#rd['baseout_dir']           = plot_dir
-#harc_plot.visualize_histograms_simple.main(rd)
+
+
