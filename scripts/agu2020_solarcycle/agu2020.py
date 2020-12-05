f6fbec054de94ee6000172371ebae2d99d29b702
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2020-12-05 13:01:54 -0500

    plotting by year

--- scripts/agu2020_solarcycle/agu2020.py
+++ scripts/agu2020_solarcycle/agu2020.py
@@ -23,7 +23,7 @@ def hash_rd(run_dict):
     return m.hexdigest()
 
 
-def load_nc_cache(rd):
+def load_nc_cache(rd,reset_cache=False):
     rd_hash     = hash_rd(rd)
     sTime_str   = rd['sTime'].strftime('%Y%m%d')
     eTime_str   = rd['eTime'].strftime('%Y%m%d')
@@ -34,6 +34,10 @@ def load_nc_cache(rd):
 
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
+
+
+    if os.path.exists(fpath) and (reset_cache is True):
+        os.remove(fpath)
 
     if os.path.exists(fpath):
         print('Loading cached file: {!s}'.format(fpath))
@@ -49,47 +53,52 @@ def load_nc_cache(rd):
 
     return nc_obj
 
-#run_name    = 'Europe'
-run_name    = 'World'
-data_dir    = os.path.join('data/solarcycle_3hr_250km/histograms',run_name)
-plot_dir    = os.path.join('output/galleries/solarcycle_3hr_250km',run_name)
-#params      = ['spot_density']
-xkeys       = ['ut_hrs','slt_mid']
-#xkeys       = ['ut_hrs']
-#sTime       = datetime.datetime(2009,1,1)
-#eTime       = datetime.datetime(2020,1,1)
+years = [2016, 2017, 2018, 2019]
 
-sTime       = datetime.datetime(2018,1,1)
-eTime       = datetime.datetime(2018,1,31)
+for year in years:
+    #run_name    = 'Europe'
+    run_name    = 'World'
+    data_dir    = os.path.join('data/solarcycle_3hr_250km/histograms',run_name)
+    plot_dir    = os.path.join('output/galleries/solarcycle_3hr_250km',run_name)
+    #params      = ['spot_density']
+    #xkeys       = ['ut_hrs','slt_mid']
+    xkeys       = ['slt_mid','ut_hrs']
+    sTime       = datetime.datetime(year,1,1)
+    eTime       = datetime.datetime(year,12,31)
+    #eTime       = datetime.datetime(2020,1,1)
 
-region      = run_name
-rgc_lim     = (0, 10000)
+    #sTime       = datetime.datetime(2009,1,1)
+    #eTime       = datetime.datetime(2009,1,31)
 
-#geo_env     = harc_plot.GeospaceEnv()
-geo_env = None
+    region      = run_name
+    rgc_lim     = (0, 10000)
 
-# Visualization ################################################################
-### Visualize Observations
-rd = {}
-rd['srcs']                  = os.path.join(data_dir,'*.data.nc.bz2')
-rd['baseout_dir']           = plot_dir
-rd['sTime']                 = sTime
-rd['eTime']                 = eTime
-rd['plot_region']           = region
-rd['geospace_env']          = geo_env
-rd['plot_sza']              = False
-rd['plot_trend']            = True 
-rd['plot_kpsymh']           = False
-rd['plot_goes']             = False
-rd['log_z']                 = False
-rd['band_keys']             = [28, 21, 14, 7, 3, 1]
-rd['xkeys']                 = xkeys
+    #geo_env     = harc_plot.GeospaceEnv()
+    geo_env = None
 
-nc = load_nc_cache(rd)
-nc.plot(**rd)
+    # Visualization ################################################################
+    ### Visualize Observations
+    rd = {}
+    rd['srcs']                  = os.path.join(data_dir,'*.data.nc.bz2')
+    rd['baseout_dir']           = plot_dir
+    rd['sTime']                 = sTime
+    rd['eTime']                 = eTime
+    rd['plot_region']           = region
+    rd['geospace_env']          = geo_env
+    rd['plot_sza']              = False
+    rd['plot_trend']            = True 
+    rd['plot_kpsymh']           = False
+    rd['plot_goes']             = False
+    rd['log_z']                 = False
+    rd['band_keys']             = [28, 21, 14, 7, 3, 1]
+    rd['xkeys']                 = xkeys
+
+    nc    = load_nc_cache(rd,reset_cache=False)
+    fpaths = nc.plot(**rd)
+
+    print()
+    for fpath in fpaths:
+        print('http://arrow.lan/~w2naf/code/harc_plot/scripts/agu2020_solarcycle/'+fpath)
+    print()
 
 import ipdb; ipdb.set_trace()
-#harc_plot.visualize_histograms.main(rd)
-#harc_plot.visualize_histograms.plot_dailies(rd)
-
-
