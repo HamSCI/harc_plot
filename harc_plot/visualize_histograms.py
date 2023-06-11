5ef736fc9d249d520082cb6511242ec8f871f8e9
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2023-06-10 23:15:38 -0400

    update deprecated frame.append to pd.concat

--- harc_plot/visualize_histograms.py
+++ harc_plot/visualize_histograms.py
@@ -241,7 +241,7 @@ class Goeser(object):
             flares          = goes.find_flares(gd['data'],min_class='M1',window_minutes=60)
             flares['sat']   = sat_nr
             gd['flares']    = flares
-            flares_combined = flares_combined.append(flares).sort_index()
+            flares_combined = pd.concat([flares_combined,flares]).sort_index()
             gd['var_tags']  = ['B_AVG']
             gd['labels']    = ['GOES {!s}'.format(sat_nr)]
 
