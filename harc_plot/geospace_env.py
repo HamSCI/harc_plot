5b0d42b4c2ff552f0f292919bfe40b2c182831e1
Kornyanat Hozumi <kornyanat.hozumi@nasa.gov>
2023-10-10 11:27:03 -0400

    Add code to succesfully pass a years parameter.

--- harc_plot/geospace_env.py
+++ harc_plot/geospace_env.py
@@ -1,6 +1,8 @@
 from .omni import Omni
 
 class GeospaceEnv(object):
-    def __init__(self):
-        self.omni   = Omni()
+#    def __init__(self):
+#        self.omni   = Omni()
+    def __init__(self, years=[2016,2017]):     # Add year parameter. Kukkai, 20231002
+        self.omni   = Omni(years=years)        # Pass year parameter. Kukkai, 20231002
         self.symh   = self.omni.symh
